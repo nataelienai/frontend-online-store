463a4e1ef988e2e8a598c4e7d25f1101b29780f6
Natã Elienai <nata.elienai@gmail.com>
2021-11-18 17:20:00 -0300

    req.4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    this.setState({ categories: await getCategories() });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <div>
+          {
+            categories.map(({ id, name }) => (
+              <label key={ id } htmlFor={ id } data-testid="category">
+                <input type="radio" name="category" id={ id } value={ name } />
+                { name }
+              </label>
+            ))
+          }
+        </div>
+      </div>
     );
   }
 }
