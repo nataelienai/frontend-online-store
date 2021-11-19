e794846e49d44dcd45560a4af802ce90eeddfe65
Gabriel A <gabrielaug4@gmail.com>
2021-11-19 14:48:02 -0300

    req.06

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import ListProducts from './ListProducts';
+import SearchButton from './SearchButton';
 
 class Home extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       categories: [],
+      list: [],
+      category: '',
+      input: '',
+      submit: false,
     };
 
     this.fetchCategories = this.fetchCategories.bind(this);
@@ -17,12 +23,28 @@ class Home extends React.Component {
     this.fetchCategories();
   }
 
+  handleClick = () => {
+    const { input, category } = this.state;
+    this.setState(async () => {
+      const getProductList = await getProductsFromCategoryAndQuery(category, input);
+      this.setState({
+        list: getProductList.results,
+        submit: true,
+      });
+    });
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   async fetchCategories() {
     this.setState({ categories: await getCategories() });
   }
 
   render() {
-    const { categories } = this.state;
+    const { categories, list, input, submit } = this.state;
 
     return (
       <div>
@@ -35,6 +57,13 @@ class Home extends React.Component {
           </Link>
         </button>
         <div>
+          <SearchButton
+            value={ input }
+            onChange={ this.handleChange }
+            onClick={ this.handleClick }
+          />
+        </div>
+        <div>
           {
             categories.map(({ id, name }) => (
               <label key={ id } htmlFor={ id } data-testid="category">
@@ -43,6 +72,9 @@ class Home extends React.Component {
               </label>
             ))
           }
+          <div>
+            {submit && <ListProducts list={ list } />}
+          </div>
         </div>
       </div>
     );
